96087bb57d1858d4d4a9c51f298ec5005888647c
Enrique Ruiz <7836092@alumnos.ufv.es>
2021-01-16 11:40:42 +0100

    Escribe archivo

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -5,13 +5,23 @@ package org.dis;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class EscribeJSON {
 
     public static void main(String[] args) {
         String json = new Gson().toJson(LeeCSV.parseaCSV("best-golf-players.csv"));
         System.out.println(json);
-
-        
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("jugadoresJson.json"));
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
  }
